5ea52e480aac18815017c8c18f569d44e911bd85
akhilesh81 <akhileshsin@gmail.com>
2018-02-08 23:02:12 +0530

    updated NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
@@ -13,21 +14,28 @@ public class NewTest {
  
 	@Test
   public void f() {
-		/*driver.get("http://demo.guru99.com/test/guru99home/");  
+		driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); */
-		System.out.println("Hello");
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+		//System.out.println("Hello");
+	}
+		@Test
+		  public void e() {
+				System.out.println("Hello");
 
   }
- /* @BeforeTest
+  @BeforeTest
   public void beforeTest() {
 	  System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Downloads\\geckodriver.exe");
-	  driver = new FirefoxDriver();
+	  final FirefoxOptions options = new FirefoxOptions();
+	  options.addPreference("security.sandbox.content.level", 5);
+	  driver = new FirefoxDriver(options);
+	  //driver = new FirefoxDriver();
   }
 
   @AfterTest
   public void afterTest() {
 	  driver.quit();
-  }*/
+  }
 
 }
